47078d374f938ee1756f83554972f3283d91fda7
manvirag <anubhavguptadec1999@gmail.com>
2020-04-16 12:32:17 +0530

    Create kth_element_BST.cpp

---
+++ kth_element_BST.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ int res;
+ int r;
+ void inorder(TreeNode* A ,int B)
+ {
+        if(!A)
+        return ;
+        
+        inorder(A->left,B);
+        res++;
+        if(res==B)
+        r=A->val;
+        inorder(A->right,B);
+        
+ }
+int Solution::kthsmallest(TreeNode* A, int B) {
+
+    res=0;
+    inorder(A,B);    
+    return r;    
+}
