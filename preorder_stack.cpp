17adf48e285ebc886924ef087dc70b6fa16f759b
manvirag <anubhavguptadec1999@gmail.com>
2020-04-16 08:37:12 +0530

    Create preorder_stack.cpp

---
+++ preorder_stack.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::preorderTraversal(TreeNode* A) {
+ 
+     vector<int> res;
+     stack<TreeNode*> st;
+     TreeNode * cur=A;
+     if(!A)
+     return res;
+     st.push(A);
+     while(!st.empty())
+     { 
+          cur=st.top();
+          res.push_back(cur->val);
+          st.pop();
+          
+          if(cur->right)
+          st.push(cur->right);
+          
+          if(cur->left)
+          st.push(cur->left);  
+     }
+    
+    return res;
+}
